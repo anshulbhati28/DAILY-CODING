788dc34f55419b9ea0da14e09dfeb2e298ec5d39
anshulbhati28 <77334580+anshulbhati28@users.noreply.github.com>
2022-08-25 15:41:32 +0530

    Time: 3 ms (89.33%), Space: 9.7 MB (55.17%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    int solve(int n,vector<int>& days, vector<int>& costs, int index, vector<int>& dp){
-        
-        if(index>=n) return 0;
-        if(dp[index]!=-1) return dp[index];
-        
-        // 1 DAY pass
-        int option1 = costs[0] + solve(n,days,costs,index+1,dp);
-        
-        int i;
-        // 7 DAY pass
-        for(i=index;i<n && days[i]< days[index]+7;i++);
-            
-        int option2 = costs[1] + solve(n,days,costs,i,dp);
-        
-        // 30 DAY pass
-        for(i=index;i<n && days[i]< days[index]+30;i++);
-            
-        int option3 = costs[2] + solve(n,days,costs,i,dp);
-        
-        return dp[index] = min(option1,min(option2,option3));
-        
-    }
+
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         int m = costs.size();
         
-        vector<int> dp(n+1,-1);
-        return solve(n,days,costs,0,dp);
+        vector<int> dp(n+1,INT_MAX);
+        dp[n] = 0;
+        for(int k=n-1;k>=0;k--){
+           
+        // 1 DAY pass
+        int option1 = costs[0] + dp[k+1];
+        
+        int i;
+        // 7 DAY pass
+        for(i=k;i<n && days[i]< days[k]+7;i++);
+            
+        int option2 = costs[1] + dp[i];
+        
+        // 30 DAY pass
+        for(i=k;i<n && days[i]< days[k]+30;i++);
+            
+        int option3 = costs[2] + dp[i];
+        
+         dp[k] = min(option1,min(option2,option3));   
+        }
+        return dp[0];
     }
 };
